dff009ef9ef9c37d04f769157f58d37fd98b1560
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-05-26 15:07:04 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+rom flask import Flask, render_template, request, redirect, url_for, session
+import os 
+
+
+#intial setup for the flask system
+app= Flask(__name__)
+app.secret_key="123123"
+
+#each app route method refers to either a page that can viewed or a backend function 
+@app.route('/')
+def home():
+  return render_template('base.html')
