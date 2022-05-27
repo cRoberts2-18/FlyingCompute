3b81483782cb862a38602a0d294b77cc60f21250
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-05-27 15:57:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
 
 @app.route('/BeginSearch/', methods = ['GET', 'POST'])
 def beginSearch():
-  nodes=request.values.get('test')
+  nodes=request.values.get('nodeArray')
   edges=request.values.get('edgeArray')
   
   return(nodes)
