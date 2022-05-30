2e3dc372e548563ffa5c4c380eb884eee792fce1
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2022-05-31 00:01:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -115,6 +115,7 @@ def connectSearch():
     pathList=os.path.join(path,"path.txt")
     f = open(pathList, "r")
     for x in f:
-      pathArr.append(x.replace("[","").replace("]","").replace(" ","").replace("\n","").replace("'",""))
+      pathArr.append(x.replace("[","").replace("]","").replace(" ","").replace("\n","").replace("'","").replace("),(",")!("))
+         
     
   return(str(pathArr))
